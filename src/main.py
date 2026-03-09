1a49bcf61ff57d572784da88dcc5e6281de4c28a
alesh <aleshus2007@gmail.com>
2026-03-09 16:03:23 +0300

    add auth router import

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI , APIRouter
+from fastapi import FastAPI, APIRouter
 
 from src.database.db import engine
 from src.models.models import Base
 from src.routers.questions.create_question import router as questions_router
 from src.routers.questions.handle_questions import router as handle_questions_router
+from src.auth.autherization import router as auth_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -23,3 +24,4 @@ async def root():
 
 app.include_router(router=questions_router)
 app.include_router(router=handle_questions_router)
+app.include_router(router=auth_router)
