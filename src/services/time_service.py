4671b64d8cbe33b35a75c38901a5725cf7b883e2
alesh <aleshus2007@gmail.com>
2026-03-14 21:07:06 +0300

    функция конвертации времени (не актуально)

--- src/services/time_service.py
+++ src/services/time_service.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+# from datetime import datetime, date, time
 
 
 def has_expired(
@@ -10,3 +10,23 @@ def has_expired(
     else:
         return True
 
+
+#
+# def change_utc_into_local_tz(
+#     utc_datetime: datetime,
+#     local_tz: str,
+# ) -> str:
+#     local_timezone = pytz.timezone(local_tz)
+#     local_datetime_str = str(utc_datetime.astimezone(local_timezone))[:-6]
+#     return local_datetime_str
+#
+#
+# def change_utc_date_and_time_into_local_tz(
+#         utc_date: date,
+#         utc_time: time,
+#         local_tz: str,
+# ) -> str:
+#     local_timezone = pytz.timezone(local_tz)
+#     utc_datetime = datetime.combine(utc_date, utc_time)
+#     local_datetime_str = utc_datetime.astimezone(local_timezone)
+#     return str(local_datetime_str)
