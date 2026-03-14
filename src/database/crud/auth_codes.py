eaa6449443bd8c61dc459b2ced0b488524fd935d
alesh <aleshus2007@gmail.com>
2026-03-14 21:03:37 +0300

    фикс бага со статусом использованных кодов

--- src/database/crud/auth_codes.py
+++ src/database/crud/auth_codes.py
@@ -6,8 +6,7 @@ from exeptions import (
     BasicOperationDatabaseError,
 )
 from src.models.models import EmailVerification
-from src.schemas.schemas import NewEmailVerificationCode
-
+from src.schemas.schemas import NewEmailVerificationCode, UserAuthSchema
 
 async def add_auth_code_to_db(
         code: NewEmailVerificationCode,
@@ -23,8 +22,11 @@ async def add_auth_code_to_db(
     return True
 
 
-async def change_auth_code_usage_status_by_user_email(email: str, used: bool, session: AsyncSession):
-    query = update(EmailVerification).where(EmailVerification.email == email).values(was_used=used)
+async def change_auth_code_usage_status_by_user_email(user_data: UserAuthSchema, used: bool, session: AsyncSession):
+    query = (update(EmailVerification)
+             .where(EmailVerification.email == user_data.email)
+             .where(EmailVerification.code == user_data.auth_code)
+             .values(was_used=used))
     try:
         await session.execute(query)
         await session.commit()
