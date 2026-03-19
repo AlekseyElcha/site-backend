218cb085dc0ca876120c16f45bafdcb4b8699e3d
AlekseyElchaninov <aleshus2007@gmail.com>
2026-03-19 12:21:07 +0300

    приведение всех email к .lower()

--- src/database/crud/users.py
+++ src/database/crud/users.py
@@ -7,12 +7,12 @@ from src.schemas.schemas import UserAddSchema
 
 
 async def get_user_role_if_user_exists_else_create_new_user(user_email: str, session: AsyncSession):
-    query = (select(AllUsers).where(AllUsers.email == user_email))
+    query = (select(AllUsers).where(AllUsers.email == user_email.lower()))
     try:
         res = await session.execute(query)
         data = res.scalars().first()
         if not data:
-            await create_new_user(user=UserAddSchema(email=user_email, role="user"), session=session)
+            await create_new_user(user=UserAddSchema(email=user_email.lower(), role="user"), session=session)
         else:
             return data.role
     except:
@@ -20,8 +20,9 @@ async def get_user_role_if_user_exists_else_create_new_user(user_email: str, ses
 
 
 async def create_new_user(user: UserAddSchema, session: AsyncSession):
-    new_user = AllUsers(email=user.email,
-                             role=user.role,
+    user_email = str(user.email).lower()
+    new_user = AllUsers(email=user_email,
+                        role=user.role,
     )
     session.add(new_user)
     try:
