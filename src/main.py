37d8bc688498ae2c5a33d466ce6f9964d8571a3e
alesh <aleshus2007@gmail.com>
2026-03-20 21:29:35 +0300

    импорт роутера работы с файлами и s3

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from src.models.models import Base
 from src.routers.questions.create_question import router as questions_router
 from src.routers.questions.handle_questions import router as handle_questions_router
 from src.auth.autherization import router as auth_router
+from src.s3.operations import router as files_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -25,6 +26,8 @@ async def root():
 app.include_router(router=questions_router)
 app.include_router(router=handle_questions_router)
 app.include_router(router=auth_router)
+app.include_router(router=files_router)
+
 
 # t0d0 передалать нейминг файлов при загрузке (время + название от абонента + уникальная последовательность) - сделано
 # TODO сделать оповещение на основную почту о создании пользователем нового обращения
