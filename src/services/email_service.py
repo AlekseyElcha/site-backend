bc92c915f81bb545c2441864012ae4ada37e7b46
alesh <aleshus2007@gmail.com>
2026-03-21 21:13:39 +0300

    функция отправки сообщения на конкрентный email c определенно

--- src/services/email_service.py
+++ src/services/email_service.py
@@ -8,7 +8,6 @@ from src.config.settings import settings
 from exeptions import SendEmailError
 
 
-
 load_dotenv()
 async def send_answer_email(user_email: str, message: str):
     fromaddr = settings.email.from_address
@@ -63,60 +62,25 @@ async def send_auth_code(user_email: str, auth_code: str):
         raise SendEmailError
 
 
-# async def send_reset_password_email_notification(user_email: str):
-#     fromaddr = "alekseyelcha07@mail.ru"
-#     toaddr = f"{user_email}"
-#     passw = os.getenv("MAIL_SERVICE_SECRET")
-#
-#     msg = MIMEMultipart()
-#     msg['From'] = fromaddr
-#     msg['To'] = toaddr
-#     msg['Subject'] = "LinkShortener // Ваш пароль был обновлён"
-#
-#     body = (f"Внимание! Ваш пароль от аккаунта {user_email} был успешно обновлён!\n"
-#             f"\n"
-#         f"Если это были не Вы, немедленно восстановите доступ: "
-#     f"Перейдите на http://localhost:8000/, нажмите на кнопку «Войти» в правом верхнем углу и нажмите на «Не помню пароль»\n"
-#     f"Далее, следуя инструкции, обновите пароль!\n"
-#     f"\n"
-#     f"В случае возникновения проблем обратитесь в поддержку, написав на этот email.")
-#     msg.attach(MIMEText(body, 'plain', 'utf-8'))
-#
-#     try:
-#         server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-#
-#         server.login(fromaddr, passw)
-#
-#         text = msg.as_string()
-#         server.sendmail(fromaddr, toaddr, text)
-#         print("Письмо успешно отправлено!")
-#
-#     except Exception as e:
-#         raise SendEmailError
+async def send_notification_with_text(email: str, subject: str , message: str):
+    fromaddr = settings.email.from_address
+    toaddr = email
+    passw = settings.email.password
 
+    msg = MIMEMultipart()
+    msg["From"] = fromaddr
+    msg["To"] = toaddr
+    msg["Subject"] = subject
+    body = message
+    msg.attach(MIMEText(body, "plain", "utf-8"))
+    try:
+        server = smtplib.SMTP_SSL(settings.email.smtp, settings.email.port)
 
+        server.login(fromaddr, passw)
 
-# async def send_email_validation(user_email: str, validate_url: str):
-#     fromaddr = "alekseyelcha07@mail.ru"
-#     toaddr = f"{user_email}"
-#     passw = os.getenv("MAIL_SERVICE_SECRET")
-#
-#     msg = MIMEMultipart()
-#     msg['From'] = fromaddr
-#     msg['To'] = toaddr
-#     msg['Subject'] = "LinkShortener // Подтвердите email для создания аккаунта"
-#
-#     body = (f"Перейдите по ссылке: {validate_url} для завершения регистрации!")
-#     msg.attach(MIMEText(body, 'plain', 'utf-8'))
-#
-#     try:
-#         server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-#
-#         server.login(fromaddr, passw)
-#
-#         text = msg.as_string()
-#         server.sendmail(fromaddr, toaddr, text)
-#         print("Письмо успешно отправлено!")
-#
-#     except Exception as e:
-#         raise SendEmailError
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        print("Письмо успешно отправлено!")
+
+    except Exception as e:
+        raise SendEmailError
