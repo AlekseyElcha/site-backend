b1d0f09d29d7574f542cb505f5dc0b26d81b7e2e
alesh <aleshus2007@gmail.com>
2026-03-21 21:08:49 +0300

    добавил отправку сообщения пользователю об успешном создании вопроса

--- src/routers/questions/create_question.py
+++ src/routers/questions/create_question.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+from email import message_from_file
 from typing import Annotated, List, Optional, Dict
 from uuid import UUID
 
@@ -10,12 +11,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from exeptions import CreateNewQuestionError, S3OperationsError
 from src.auth.code_generator import generate_auth_code_and_and_to_db
 from src.auth.utils import validate_auth_user
+from src.config.settings import settings
 from src.database.crud.questions import create_new_question, get_question_by_uuid
 from src.database.db import get_session
 from src.models.models import Questions
 from src.s3.operations import upload_multiple_files
 from src.schemas.schemas import UserSchema, NewQuestionSchema, UserAuthSchema
 from src.auth.autherization import auth_user_check_self_info
+from src.services.email_service import send_notification_with_text
 
 router = APIRouter(
     prefix="/questions",
@@ -58,6 +61,31 @@ async def create_question(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Ошибка при загрузке!"
             )
+    try:
+        # TODO поменять ссылки здесь!!!
+        await send_notification_with_text(
+            email=question_data.email,
+            subject="ООО «Домофон-сервис». Уведомление о создании обращения",
+            message=f"Уважаемый {question_data.name} {question_data.surname},\n"
+                    f"Уведомляем Вас о том, что Вы успешно создали обращение в ООО «Домофон-сервис» "
+                    f"при помощи нашего онлайн-сервиса <ССЫЛКУ СЮДА> \n\n"
+                    f"Если Вы не оставляли обращение, напишите <СЮДА> - со всем разберёмся.\n"
+                    f"Уникальный идентификатор вопроса: {unique_id}\n\n"
+                    f"Данное письмо было отправлено автоматически, просьба не отвечать на него."
+        )
+    except:
+        print("Ошибка при отправке нотификации user, но вопрос создан")
+    try:
+        await send_notification_with_text(
+            email=settings.email.from_address,
+            subject="Уведомление о новом обращении на платформе",
+            message=f"Было создано новое обращение на платформе.\n"
+                    f"Ссылка на обращение: http://localhost:3000/admin/{unique_id}\n\n"
+                    f"Базовая информация об обращении:\n"
+                    f"{question_data}"
+        )
+    except:
+        print("Ошибка при отправке нотификации admin, но вопрос создан")
     return {
         "message": "Вопрос успешно создан."
     }
