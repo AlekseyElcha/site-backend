16026ef51c0cdc4ccf8b3cb4a4f04c7c50028640
alesh <aleshus2007@gmail.com>
2026-04-12 20:53:28 +0300

    переделка ретраев, обноление логирования

--- src/routers/questions/create_question.py
+++ src/routers/questions/create_question.py
@@ -50,7 +50,7 @@ async def create_question(
     try:
         await create_new_question(question_data, unique_id, session)
     except CreateNewQuestionError:
-        logger.warning("ERROR WHILE CREATING NEW QUESTION")
+        logger.warning("ОШИБКА ПРИ СОЗДАНИИ ВОПРОСА!")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Произошла ошибка при создании вопроса.",
@@ -62,7 +62,7 @@ async def create_question(
                 question_uuid=unique_id,
             )
         except Exception as e:
-            logger.warning("ERROR WHILE UPLOADING FILES {}".format(e))
+            logger.warning("ОШИБКА ПРИ ЗАГРУЗКЕ ФАЙЛОВ {}".format(e))
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Ошибка при загрузке!"
@@ -77,32 +77,32 @@ async def create_question(
             ),
         )
     except SendEmailError:
-        success = False
+        # success = False
         retries_count = settings.email.retries_for_sending_messages
         for retry in range(retries_count):
             logger.warning(
-                "ERROR WHILE SENDING USER CREATED QUESTION: {}."
-                " RETRYING AFTER 5 SECONDS"
+                "Ошибка при отправке email на: {}."
+                " об успешном создании обращения."
                 .format(question_data.email)
             )
-            await asyncio.sleep(5)
-            try:
-                await send_notification_with_text(
-                    email=question_data.email.lower(),
-                    subject="ООО «Домофон-сервис». Уведомление о создании обращения",
-                    message=settings.business.format_message_text(
-                        question_data.name,
-                        question_data.surname,
-                        unique_id
-                    ),
-                )
-                success = True
-            except SendEmailError:
-                logger.warning(
-                    "RETRY {} UNSUCCESSFUL. RETRYING AFTER 5 SECONDS".format(retry+1)
-                )
-        if not success:
-            logger.warning("{} RETRIES UNSUCCESSFUL.".format(retries_count))
+        #     await asyncio.sleep(5)
+        #     try:
+        #         await send_notification_with_text(
+        #             email=question_data.email.lower(),
+        #             subject="ООО «Домофон-сервис». Уведомление о создании обращения",
+        #             message=settings.business.format_message_text(
+        #                 question_data.name,
+        #                 question_data.surname,
+        #                 unique_id
+        #             ),
+        #         )
+        #         success = True
+        #     except SendEmailError:
+        #         logger.warning(
+        #             "RETRY {} UNSUCCESSFUL. RETRYING AFTER 5 SECONDS".format(retry+1)
+        #         )
+        # if not success:
+        #     logger.warning("{} RETRIES UNSUCCESSFUL.".format(retries_count))
     try:
         await send_notification_with_text(
             email=settings.email.from_address.lower(),
@@ -112,27 +112,26 @@ async def create_question(
             )
         )
     except:
-        success = False
-        logger.warning("ERROR WHILE SENDING QUESTION CREATED TO ADMIN. RETRYING AFTER 5 SEC.")
-        await asyncio.sleep(5)
-        retries_count = settings.email.retries_for_sending_messages
-        for retry in range(retries_count):
-            try:
-                await send_notification_with_text(
-                    email=settings.email.from_address.lower(),
-                    subject="Уведомление о новом обращении на платформе",
-                    message=settings.business.format_message_text_for_admins(
-                        unique_id, question_data
-                    )
-                )
-                success = True
-            except SendEmailError:
-                logger.warning(
-                    "RETRY {} UNSUCCESSFUL. RETRYING AFTER 5 SECONDS".format(retry+1)
-                )
-            if not success:
-                logger.warning("{} RETRIES UNSUCCESSFUL.".format(retries_count))
-
+        # success = False
+        logger.warning("Ошибка при отправке сообщения о новом обращении админам.")
+        # await asyncio.sleep(5)
+        # retries_count = settings.email.retries_for_sending_messages
+        # for retry in range(retries_count):
+        #     try:
+        #         await send_notification_with_text(
+        #             email=settings.email.from_address.lower(),
+        #             subject="Уведомление о новом обращении на платформе",
+        #             message=settings.business.format_message_text_for_admins(
+        #                 unique_id, question_data
+        #             )
+        #         )
+        #         success = True
+        #     except SendEmailError:
+        #         logger.warning(
+        #             "RETRY {} UNSUCCESSFUL. RETRYING AFTER 5 SECONDS".format(retry+1)
+        #         )
+        #     if not success:
+        #         logger.warning("{} RETRIES UNSUCCESSFUL.".format(retries_count))
 
     return {
         "message": "Вопрос успешно создан."
