6e11b146b13fbd242ae29300844ff1893d186b32
AlekseyElchaninov <aleshus2007@gmail.com>
2026-04-22 20:56:08 +0300

    рефакторинг импортов в связи с ренеймингом файлов конфига

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,18 +2,18 @@ from pathlib import Path
 
 from pydantic_settings import  BaseSettings, SettingsConfigDict
 
-from src.config.app import AppConfig
-from src.config.business import Business
-from src.config.db import DatabaseConfig
-from src.config.email import EmailServiceConfig
-from src.config.jwt import AuthJWT
-from src.config.logs import LoggingConfig
-from src.config.redis import RedisConfig
-from src.config.s3 import S3Settings
+from src.config.app_config import AppConfig
+from src.config.business_config import Business
+from src.config.db_config import DatabaseConfig
+from src.config.email_config import EmailServiceConfig
+from src.config.jwt_config import AuthJWT
+from src.config.logs_config import LoggingConfig
+from src.config.redis_config import RedisConfig
+from src.config.s3_config import S3Settings
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict()
+    model_config = SettingsConfigDict(env_file=".env", extra='ignore')
 
     app: AppConfig = AppConfig()
     db: DatabaseConfig = DatabaseConfig()
