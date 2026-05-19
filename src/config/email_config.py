74602a884983bffd555623ce072c7086b8b2f0de
alesh <aleshus2007@gmail.com>
2026-05-19 19:05:52 +0300

    обноленная функция отправки текстового email (интеграция Resend API)

--- src/config/email_config.py
+++ src/config/email_config.py
@@ -4,11 +4,19 @@ from pydantic import BaseModel
 
 load_dotenv()
 
+
 class EmailServiceConfig(BaseModel):
+    # неактуально, т.к. smtp-порты заблокированы на VPS
     from_address: str = os.getenv("MAIL_FROM_ADDRESS")
     password: str = os.getenv("MAIL_SERVICE_SECRET")
     smtp: str = "smtp.mail.ru"
     port: int = 587
+
+    # обход блокировки портов на VPS через "Resend"
+    resend_api_key = os.getenv("RESEND_API_KEY")
+
+    from_email: str = "ООО \"Домофон-сервис\" <noreply@domofon-servis-odi.ru>"
+
 
     # TODO Поменять!!!!!!!
     url_to_personal_account: str = "http://test.domofon-servis-odi.ru/user/"
@@ -27,4 +35,3 @@ class EmailServiceConfig(BaseModel):
     send_notification_with_text_retries: int = 2
     send_notification_with_text_delay: float = 1.0
     send_notification_with_text_backoff: float = 2.0
-
